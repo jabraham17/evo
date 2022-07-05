97936e324c665b74e0f9d4555adf4dda02450c70
Jacob Abraham <jacob.r.abraham@gmail.com>
2022-07-04 17:32:11 -0700

    more formatting

--- src/dot/dot.h
+++ src/dot/dot.h
@@ -32,23 +32,14 @@ bool dot_set_attribute_on_vertex(
     unsigned long id,
     char* key,
     char* value);
-char* dot_get_attribute_on_vertex(
-    dgraph_t* graph,
-    unsigned long id,
-    char* key);
-bool dot_has_attribute_on_vertex(
-    dgraph_t* graph,
-    unsigned long id,
-    char* key);
+char* dot_get_attribute_on_vertex(dgraph_t* graph, unsigned long id, char* key);
+bool dot_has_attribute_on_vertex(dgraph_t* graph, unsigned long id, char* key);
 bool dot_remove_attribute_on_vertex(
     dgraph_t* graph,
     unsigned long id,
     char* key);
 
-bool dot_add_edge(
-    dgraph_t* graph,
-    unsigned long first,
-    unsigned long second);
+bool dot_add_edge(dgraph_t* graph, unsigned long first, unsigned long second);
 bool dot_set_attribute_on_edge(
     dgraph_t* graph,
     unsigned long first,
