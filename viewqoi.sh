5731d786e4ec71bb3c6d84c9e6a29cb23998df8d
jabraham17 <jademabraham17@gmail.com>
2023-05-31 07:03:03 -0700

    make script better
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- viewqoi.sh
+++ viewqoi.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-FILEPATH=$1
-if [ -z $FILEPATH ]; then
-  echo "usage ./viewqoi <qoi file>"
-  exit 1
-fi
-FILENAME=$(basename -- "$FILEPATH")
-EXT="${FILENAME##*.}"
-if [ $EXT != "qoi" ]; then 
-  echo "'$FILEPATH' has invalid extension"
+if [ $# -eq 0 ]; then
+  echo "usage ./viewqoi <qoi file>..."
   exit 1
 fi
 
-QOICONV=./build/qoiconv
-if [ ! -z $2 ]; then
-  QOICONV=$2
-fi
+QOICONV_=${QOICONV:-./build/qoiconv}
 
-OUT=`mktemp --suffix=.png`
-$QOICONV $FILEPATH $OUT
-/usr/bin/xdg-open $OUT
+for FILEPATH in "$@"; do
+
+  FILENAME=$(basename -- "$FILEPATH")
+  EXT="${FILENAME##*.}"
+  FILENAME="${FILENAME%.*}"
+  if [ $EXT != "qoi" ]; then 
+    echo "'$FILEPATH' has invalid extension"
+    exit 1
+  fi
+
+  OUT=`mktemp -p /tmp $FILENAME.XXX --suffix=.png`
+  $QOICONV_ $FILEPATH $OUT
+  /usr/bin/xdg-open $OUT
+done
